bc2a9cd28e3f2224d747e871060f5876bbdafa0c
nghiale <trongnghialedinh@gmail.com>
2022-04-29 15:03:40 +0700

    composite key class for ProductSize table

--- back-end/src/main/java/com/example/demo/entities/idclasses/ProductSizeId.java
+++ back-end/src/main/java/com/example/demo/entities/idclasses/ProductSizeId.java
@@ -1,11 +1,8 @@
 package com.example.demo.entities.idclasses;
 
-import com.example.demo.entities.ProductEntity;
-import com.example.demo.entities.SizeEntity;
-
 import java.io.Serializable;
 
 public class ProductSizeId implements Serializable {
-    private ProductEntity product;
-    private SizeEntity size;
+    private Long product;
+    private Long size;
 }
