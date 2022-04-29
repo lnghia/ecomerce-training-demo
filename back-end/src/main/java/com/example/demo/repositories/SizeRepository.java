d7e4dada28ce39bb11919e95b7a2b4d594d7c232
nghiale <trongnghialedinh@gmail.com>
2022-04-29 15:11:27 +0700

    size service and repository

---
+++ back-end/src/main/java/com/example/demo/repositories/SizeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.SizeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SizeRepository extends JpaRepository<SizeEntity, Long> {
+}
---
+++ back-end/src/main/java/com/example/demo/services/interfaces/SizeService.java
@@ -0,0 +1,7 @@
+package com.example.demo.services.interfaces;
+
+import com.example.demo.entities.SizeEntity;
+
+public interface SizeService {
+    SizeEntity findById(Long id);
+}
