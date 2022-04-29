f03a8fa82ee50caff45571e6d6ab721ce57cd827
nghiale <trongnghialedinh@gmail.com>
2022-04-29 15:12:28 +0700

    gender service and repository

---
+++ back-end/src/main/java/com/example/demo/repositories/GenderRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.GenderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenderRepository extends JpaRepository<GenderEntity, Long> {
+
+}
