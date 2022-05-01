610b0a49357024a8955fa7951ed99128e7da3033
nghiale <trongnghialedinh@gmail.com>
2022-05-01 13:37:30 +0700

    avoid circular referenced objects

--- back-end/src/main/java/com/example/demo/configurations/modelmapper/ModelMapperConfigurer.java
+++ back-end/src/main/java/com/example/demo/configurations/modelmapper/ModelMapperConfigurer.java
@@ -1,7 +1,10 @@
 package com.example.demo.configurations.modelmapper;
 
+import com.example.demo.dto.responses.ProductSizeResponseDto;
+import com.example.demo.entities.ProductSizeEntity;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +17,19 @@ public class ModelMapperConfigurer {
 
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        modelMapper.getConfiguration().setPreferNestedProperties(true);
+
+        resolveCircularReferencedBetweenProductAndProductSizeWhenMapping(modelMapper);
 
         return modelMapper;
+    }
+
+    public void resolveCircularReferencedBetweenProductAndProductSizeWhenMapping(ModelMapper modelMapper) {
+        modelMapper.addMappings(new PropertyMap<ProductSizeEntity, ProductSizeResponseDto>() {
+            @Override
+            protected void configure() {
+                skip(destination.getProduct());
+            }
+        });
     }
 }
