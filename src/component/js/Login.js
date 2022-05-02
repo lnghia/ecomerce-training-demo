73b217b0eb84a9650133e317d816076c09375721
nghiale <trongnghialedinh@gmail.com>
2022-05-02 17:56:06 +0700

    implement login

---
+++ src/component/js/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import "../css/Login.css";
+import { login } from "../../api/Authentication";
+
+
+const Login = (props) => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const navigate = useNavigate();
+
+    const onSubmit = async data => {
+        let loginSuccess = await login(data.email, data.password);
+
+        if(loginSuccess) {
+            navigate('/');
+        }
+    }
+
+    return (
+        <div>
+            <div className="sidenav">
+                {/* <img src="/assets/bg.jpg" className="card-img" /> */}
+                {/* <div className="hero">
+                    <img src="/assets/bg.jpg" className="card-img" />
+                    <div className="card-img-overlay">
+                        <h2>E Shopper<br /> Login Page</h2>
+                    </div>
+                </div> */}
+            </div>
+            <div className="main">
+                <div className="col-md-6 col-sm-12">
+                    <div className="login-form">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input {...register("email", { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g })} type="text" className="form-control" placeholder="Email" />
+                                {errors.email?.type === 'required' && "Email is required"}
+                                {errors.email?.type === 'pattern' && "Invalid email"}
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input {...register("password", { required: true })} type="password" className="form-control" placeholder="Password" />
+                                {errors.password?.type === 'required' && "Password is required"}
+                            </div>
+                            <button type="submit" className="btn btn-dark">Login</button>
+                            <button type="submit" className="btn btn-secondary ml-2">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ import Footer from './component/js/Footer';
 import Categories from './component/js/Categories';
 import Features from './component/js/Features';
 import NavBar from './component/js/NavBar';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import 'toastr/build/toastr.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <TopBar />
-    <NavBar />
-    <Features />
+  <BrowserRouter>
     <App />
-    <Footer />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
