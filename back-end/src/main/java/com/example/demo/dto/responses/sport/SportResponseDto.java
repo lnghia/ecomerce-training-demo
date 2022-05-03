fc11407e303ca84a959637373982e967fa58d7b0
nghiale <trongnghialedinh@gmail.com>
2022-05-03 22:36:08 +0700

    get all sport

---
+++ back-end/src/main/java/com/example/demo/dto/responses/sport/SportResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto.responses.sport;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SportResponseDto {
+    private Long id;
+
+    private String name;
+}
