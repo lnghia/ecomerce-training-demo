2538e8f19a2a11fa101fae7c9deeac52e7ea18ca
nghiale <trongnghialedinh@gmail.com>
2022-05-03 22:32:36 +0700

    get all category service

--- back-end/src/main/java/com/example/demo/services/implementations/category/CategoryCrudServiceImpl.java
+++ back-end/src/main/java/com/example/demo/services/implementations/category/CategoryCrudServiceImpl.java
@@ -1,24 +1,30 @@
 package com.example.demo.services.implementations.category;
 
+import com.example.demo.dto.responses.category.CategoryResponseDto;
 import com.example.demo.entities.CategoryEntity;
 import com.example.demo.exceptions.CategoryNotFoundException;
 import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.services.interfaces.category.CategoryCrudService;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @NoArgsConstructor
 public class CategoryCrudServiceImpl implements CategoryCrudService {
     private CategoryRepository categoryRepository;
 
+    private ModelMapper modelMapper;
+
     @Autowired
-    public CategoryCrudServiceImpl(CategoryRepository categoryRepository) {
+    public CategoryCrudServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper) {
         this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -31,5 +37,17 @@ public class CategoryCrudServiceImpl implements CategoryCrudService {
         }
 
         return categoryEntities;
+    }
+
+    @Override
+    public List<CategoryResponseDto> findAll() {
+        List<CategoryEntity> categoryEntities = categoryRepository.findAll();
+        List<CategoryResponseDto> result = null;
+
+        result = categoryEntities.stream().map(categoryEntity -> {
+            return modelMapper.map(categoryEntity, CategoryResponseDto.class);
+        }).collect(Collectors.toList());
+
+        return result;
     }
 }
--- back-end/src/main/java/com/example/demo/services/interfaces/category/CategoryCrudService.java
+++ back-end/src/main/java/com/example/demo/services/interfaces/category/CategoryCrudService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services.interfaces.category;
 
+import com.example.demo.dto.responses.category.CategoryResponseDto;
 import com.example.demo.entities.CategoryEntity;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.Set;
 
 public interface CategoryCrudService {
     List<CategoryEntity> findByIds(Set<Long> ids);
+
+    List<CategoryResponseDto> findAll();
 }
