c1b4ffb2eac702f07ea12d5c39ccf2a64c1c50db
nghiale <trongnghialedinh@gmail.com>
2022-05-03 22:36:20 +0700

    get all gender

---
+++ back-end/src/main/java/com/example/demo/controllers/GenderController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.responses.ResponseBodyDto;
+import com.example.demo.dto.responses.gender.GenderResponseDto;
+import com.example.demo.services.interfaces.gender.GenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/gender")
+public class GenderController {
+    @Autowired
+    private GenderService genderService;
+
+    @GetMapping
+    public ResponseEntity<ResponseBodyDto> getAll() {
+        List<GenderResponseDto> genderResponseDtoList = genderService.findAll();
+        ResponseBodyDto responseData = ResponseBodyDto.builder().data(genderResponseDtoList).build();
+
+        return ResponseEntity.ok(responseData);
+    }
+}
