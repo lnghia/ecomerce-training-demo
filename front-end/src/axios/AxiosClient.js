3fde365fee2932fbf34240a39ef66f8ff0e653ea
nghiale <trongnghialedinh@gmail.com>
2022-05-03 22:28:38 +0700

    setup axios interceptor

--- front-end/src/axios/AxiosClient.js
+++ front-end/src/axios/AxiosClient.js
@@ -5,4 +5,23 @@ const AxiosClient = axios.create({
     baseURL: "http://localhost:8080/api"
 });
 
+AxiosClient.interceptors.request.use(
+    config => {
+        const accessToken = window.localStorage.getItem("accessToken");
+        const refreshToken = window.localStorage.getItem("refreshToken");
+        if (accessToken) {
+            config.headers['Authorization'] = 'Bearer' + accessToken;
+        }
+        if (refreshToken) {
+            config.headers['Refresh-Token'] = refreshToken;
+        }
+        config.headers['Content-Type'] = 'application/json';
+
+        return config;
+    },
+    error => {
+        Promise.reject(error);
+    }
+)
+
 export default AxiosClient;
