b424742594b44b0c67210e3b14015f346aceb46b
nghiale <trongnghialedinh@gmail.com>
2022-05-03 22:29:31 +0700

    update

--- front-end/src/component/js/Home.js
+++ front-end/src/component/js/Home.js
@@ -3,12 +3,14 @@ import TopBar from "./TopBar";
 import NavBar from "./NavBar";
 import Features from "./Features";
 import Footer from "./Footer";
+import FeaturedProducts from "./FeaturedProducts";
 
 const Home = () => {
     return (
         <>
             <TopBar />
             <NavBar />
+            <FeaturedProducts />
             <Features />
             <Footer />
         </>
