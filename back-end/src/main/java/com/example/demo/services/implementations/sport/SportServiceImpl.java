f089726fec1695fa4b6c658445c68489e87f26fb
nghiale <trongnghialedinh@gmail.com>
2022-05-03 22:33:22 +0700

    get all sport service

--- back-end/src/main/java/com/example/demo/services/implementations/sport/SportServiceImpl.java
+++ back-end/src/main/java/com/example/demo/services/implementations/sport/SportServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.demo.services.implementations.sport;
 
+import com.example.demo.dto.responses.sport.SportResponseDto;
 import com.example.demo.entities.SportEntity;
 import com.example.demo.repositories.SportRepository;
 import com.example.demo.services.interfaces.sport.SportService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Service
 public class SportServiceImpl implements SportService {
     private ModelMapper modelMapper;
 
@@ -22,5 +27,17 @@ public class SportServiceImpl implements SportService {
     @Override
     public Optional<SportEntity> findById(Long id) {
         return sportRepository.findById(id);
+    }
+
+    @Override
+    public List<SportResponseDto> findAll() {
+        List<SportEntity> sportEntities = sportRepository.findAll();
+        List<SportResponseDto> result = null;
+
+        result = sportEntities.stream().map(sportEntity -> {
+            return modelMapper.map(sportEntity, SportResponseDto.class);
+        }).collect(Collectors.toList());
+
+        return result;
     }
 }
--- back-end/src/main/java/com/example/demo/services/interfaces/sport/SportService.java
+++ back-end/src/main/java/com/example/demo/services/interfaces/sport/SportService.java
@@ -1,9 +1,13 @@
 package com.example.demo.services.interfaces.sport;
 
+import com.example.demo.dto.responses.sport.SportResponseDto;
 import com.example.demo.entities.SportEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SportService {
     Optional<SportEntity> findById(Long id);
+
+    List<SportResponseDto> findAll();
 }
