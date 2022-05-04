e683606a9097113b965aa07245c610a42f0d8bb7
nghiale <trongnghialedinh@gmail.com>
2022-05-04 22:35:34 +0700

    add selectedProduct selector

--- front-end/src/redux/selectors.js
+++ front-end/src/redux/selectors.js
@@ -1,3 +1,5 @@
 export const loginSuccessSelector = state => state.authentication.loginSuccess;
 
 export const productListSelector = state => state.product.productList;
+
+export const selectedProductIdSelector = state => state.product.selectedProduct;
