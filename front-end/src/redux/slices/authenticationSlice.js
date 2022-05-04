c6fff81eee072c2302aaf24babc44061d4221c44
nghiale <trongnghialedinh@gmail.com>
2022-05-04 22:35:00 +0700

    save tokens to localStorage

--- front-end/src/redux/slices/authenticationSlice.js
+++ front-end/src/redux/slices/authenticationSlice.js
@@ -3,14 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authenticationSlice = createSlice({
     name: 'loggedInIdentity',
     initialState: {
-        loginSuccess: false
+        loginSuccess: false,
+        accessToken: "",
+        refreshToken: "",
     },
     reducers: {
         loginAction: state => {
             state.loginSuccess = true;
+            state.accessToken = window.localStorage.getItem("accessToken");
+            state.refreshToken = window.localStorage.getItem("refreshToken");
         },
         logoutAction: state => {
             state.loginSuccess = false;
+            state.accessToken = "";
+            state.refreshToken = "";
         }
     }
 })
