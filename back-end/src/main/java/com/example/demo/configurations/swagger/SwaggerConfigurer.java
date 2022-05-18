04f52cda9896587fab694d470ab658ff0e953853
nghiale <trongnghialedinh@gmail.com>
2022-05-19 00:32:31 +0700

    refactor swagger config and update permitted swagger endpoints

--- back-end/src/main/java/com/example/demo/configurations/swagger/SwaggerConfigurer.java
+++ back-end/src/main/java/com/example/demo/configurations/swagger/SwaggerConfigurer.java
@@ -11,7 +11,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -25,12 +24,13 @@ public class SwaggerConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .pathMapping("/");
     }
 
     private SecurityContext securityContext() {
@@ -41,6 +41,6 @@ public class SwaggerConfigurer {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 }
